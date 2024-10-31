6bae933d2dd2a6d47b8180d856f1c76ce5a205c4
Oliver-Models <oliver.modeling@gmail.com>
2024-10-30 23:58:29 -0400

    Create calculator.py
    added required variables for simulation after research.

---
+++ calculator.py
@@ -0,0 +1,28 @@
+import numpy
+
+#adjustable laser variables. add feature at a later date.
+def vcselComponents():
+
+def laserWavelength():
+
+def m2():
+
+def vcselToMmfAngle():
+
+def acceptanceCone():
+
+#adjustable mmf variables. add feature sooner
+
+#just the glass?
+def materialRefractionIndex():
+
+#include industry standards
+def mmfCircumference():
+
+def mmfLength():
+
+def temperature():
+
+def noise():
+
+#Ideal BER calculations. Healthy variance.
