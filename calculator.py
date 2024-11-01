a8928f344f48360f0e1b411eb727d5c2d0b76a3f
Oliver-Models <oliver.modeling@gmail.com>
2024-10-31 23:59:01 -0400

    Update calculator.py
    added equation flow, and variables required to calculate Sellmeier's equations and Snell's critical angle.

--- calculator.py
+++ calculator.py
@@ -1,9 +1,32 @@
-import numpy
+import numpy as np
+import scipy as sp
+import math
 
 #adjustable laser variables. add feature at a later date.
-def vcselComponents():
+#def vcselComponents():
 
-def laserWavelength():
+#gather laser wavelength
+wavelength = input("Enter Laser Wavelength, um")
+
+
+#Sellmeier's Equation, SiO2 (MMF Core). Solve using input wavelength
+def coreRefractionIndex(wavelength):
+    temporary = temporary
+    for
+
+
+#Sellmeier's Equation, SiO2 + B2O2 Dopant (Outer cladding)
+def cladRefractionIndex(wavelength):
+    temporary = temporary
+    for
+
+#Entrance angle adjustment
+def snellsLaw(coreRefractionIndex, cladRefractionIndex):
+    SLE = coreRefractionIndex.sin() 
+
+#Critical Angel Required for total reflection
+def snellsCriticalAngle():
+
 
 def m2():
 
@@ -11,10 +34,14 @@ def vcselToMmfAngle():
 
 def acceptanceCone():
 
+
+
+def speedOverTime():
+
 #adjustable mmf variables. add feature sooner
 
-#just the glass?
-def materialRefractionIndex():
+#sio2
+
 
 #include industry standards
 def mmfCircumference():
