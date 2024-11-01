5095fc406d5e8ae80f729f9c9e69c50b06784ed6
Oliver-Models <oliver.modeling@gmail.com>
2024-11-01 09:17:52 -0400

    Update calculator.py
    Added Sellmeier's equation in algorithm form. Added Si02 coefficients

--- calculator.py
+++ calculator.py
@@ -1,20 +1,29 @@
 import numpy as np
-import scipy as sp
 import math
 
 #adjustable laser variables. add feature at a later date.
 #def vcselComponents():
 
 #gather laser wavelength
-wavelength = input("Enter Laser Wavelength, um")
+wavelength_str = input("Enter Laser Wavelength, um: ")
+wavelength = int(wavelength_str)
 
+b_array = [0.6961663,0.4079426,0.8974794]
+c_array_root = [0.0684043,0.1162414,9.896161]
 
 #Sellmeier's Equation, SiO2 (MMF Core). Solve using input wavelength
 def coreRefractionIndex(wavelength):
-    temporary = temporary
-    for
+    contributions = []
+    for b, c in zip(b_array, c_array_root):
+        calculate_term = (b * wavelength**2) / (wavelength**2 - c**2)
+        contributions.append(calculate_term)
+    core_refraction = 1 + sum(contributions)
+    n = np.sqrt(core_refraction)
+    return f"a wavelength of {wavelength} um has a refraction index of {n}"
+print(coreRefractionIndex(wavelength))
 
 
+"""
 #Sellmeier's Equation, SiO2 + B2O2 Dopant (Outer cladding)
 def cladRefractionIndex(wavelength):
     temporary = temporary
@@ -53,3 +62,4 @@ def temperature():
 def noise():
 
 #Ideal BER calculations. Healthy variance.
+"""
