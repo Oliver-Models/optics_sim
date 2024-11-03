683c1894c6dc4c9cf6a8186b6a93467635c97fec
Oliver-Models <oliver.modeling@gmail.com>
2024-11-03 00:49:38 -0400

    Update calculator.py
    added critical angles and GeO2 coefficients

--- calculator.py
+++ calculator.py
@@ -1,40 +1,62 @@
 import numpy as np
-import math
 
-#adjustable laser variables. add feature at a later date.
-#def vcselComponents():
+# gather laser wavelength
+wavelength_str = input("Enter Laser Wavelength, 210-6700 um: ")
+wavelength = float(wavelength_str)  # Use float instead of int
 
-#gather laser wavelength
-wavelength_str = input("Enter Laser Wavelength, um: ")
-wavelength = int(wavelength_str)
+# SiO2 coefficients
+b1_array = [0.6961663,0.4079426,0.8974794]
+c1_array_root = [0.0684043,0.1162414,9.896161]
 
-b_array = [0.6961663,0.4079426,0.8974794]
-c_array_root = [0.0684043,0.1162414,9.896161]
 
-#Sellmeier's Equation, SiO2 (MMF Core). Solve using input wavelength
+b2_array = [0.7072363,0.4389641,0.8931482]
+c2_array = [0.0684611,0.1200138,10.090738]
+
+
+#Sellmeier's Equation, SiO2 (Fiber Core). Solve using input wavelength
 def coreRefractionIndex(wavelength):
     contributions = []
-    for b, c in zip(b_array, c_array_root):
+    for b, c in zip(b1_array, c1_array_root):
+        #perform operations and append to array
         calculate_term = (b * wavelength**2) / (wavelength**2 - c**2)
         contributions.append(calculate_term)
+    #complete operation and root    
     core_refraction = 1 + sum(contributions)
     n = np.sqrt(core_refraction)
-    return f"a wavelength of {wavelength} um has a refraction index of {n}"
+    return n
 print(coreRefractionIndex(wavelength))
 
 
-"""
+
+
+#FIX!!!!
 #Sellmeier's Equation, SiO2 + B2O2 Dopant (Outer cladding)
 def cladRefractionIndex(wavelength):
-    temporary = temporary
-    for
-
-#Entrance angle adjustment
-def snellsLaw(coreRefractionIndex, cladRefractionIndex):
-    SLE = coreRefractionIndex.sin() 
+    contributions2 = []
+    for b2, c2 in zip(b2_array, c2_array):
+        #perform operations and append
+         calculate_term2 = (b2 * wavelength**2) / (wavelength**2 - c2**2)
+         contributions2.append(calculate_term2)
+         clad_refraction = 1 + sum(contributions2)
+         u = np.sqrt(clad_refraction)
+         return u
+print(cladRefractionIndex(wavelength))
 
 #Critical Angel Required for total reflection
-def snellsCriticalAngle():
+def snellsCriticalAngle(coreRefractionIndex, cladRefractionIndex):
+    critical_angle = np.arcsin(cladRefractionIndex(wavelength)/coreRefractionIndex(wavelength))
+    ca = critical_angle
+    return f" the critical angle is {ca}"
+print(snellsCriticalAngle(coreRefractionIndex,cladRefractionIndex))
+
+
+#Entrance angle adjustment
+#def snellsLaw(coreRefractionIndex, cladRefractionIndex):
+
+
+"""
+
+
 
 
 def m2():
